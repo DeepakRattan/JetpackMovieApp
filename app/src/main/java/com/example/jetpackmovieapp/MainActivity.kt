2e4da13bfe97548d6eeffca8c13036a652cda403
Deepak Rattan <deepak.rattan@itechnolabs.tech>
2023-04-05 17:11:39 +0530

    Implements LazyColumn to show list of movies

--- app/src/main/java/com/example/jetpackmovieapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpackmovieapp/MainActivity.kt
@@ -3,11 +3,19 @@ package com.example.jetpackmovieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackmovieapp.ui.theme.JetpackMovieAppTheme
@@ -43,10 +51,60 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
-fun MainContent() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text("Hello")
+fun MainContent(
+    movieList: List<String> = listOf(
+        "Lagaan",
+        "Avtaar",
+        "Bahubali",
+        "RRR",
+        "Chak De",
+        "Dhoni",
+        "Bend it like Bekhem",
+        "DDLJ"
+    )
+) {
+    Column(modifier = Modifier.padding(all = 12.dp)) {
+        LazyColumn {
+            items(items = movieList) {
+                MovieRow(movie = it)
+            }
+        }
+
     }
+}
+
+@Composable
+fun MovieRow(movie: String) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp),
+        shape = RoundedCornerShape(
+            corner = CornerSize(16.dp)
+        ),
+        elevation = 6.dp
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                elevation = 4.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Image")
+            }
+
+            Text(text = movie)
+
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
