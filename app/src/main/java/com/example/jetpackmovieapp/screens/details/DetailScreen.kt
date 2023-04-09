7653b9cc7e43ea9ac8e1b85866feaaea8db1226c
Deepak Rattan <deepak.rattan@itechnolabs.tech>
2023-04-09 09:40:33 +0530

    Handles back navigation on clicking back button

--- app/src/main/java/com/example/jetpackmovieapp/screens/details/DetailScreen.kt
+++ app/src/main/java/com/example/jetpackmovieapp/screens/details/DetailScreen.kt
@@ -1,15 +1,15 @@
 package com.example.jetpackmovieapp.screens.details
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 /**
@@ -31,6 +31,13 @@ fun DetailsScreen(navController: NavController, movie: String?) {
                 text = "Movie is $movie",
                 style = MaterialTheme.typography.h5
             )
+            Spacer(modifier = Modifier.height(20.dp))
+            Button(
+                onClick = {
+                    navController.popBackStack()
+                }) {
+                Text(text = "Go Back")
+            }
         }
 
     }
