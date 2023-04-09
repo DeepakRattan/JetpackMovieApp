7369b5e439d7edc9457d3a448ea64ca4e9a797c0
Deepak Rattan <deepak.rattan@itechnolabs.tech>
2023-04-09 14:40:39 +0530

    Uses Coil Image URL Loading library

--- app/src/main/java/com/example/jetpackmovieapp/widgets/Widgets.kt
+++ app/src/main/java/com/example/jetpackmovieapp/widgets/Widgets.kt
@@ -1,28 +1,33 @@
 package com.example.jetpackmovieapp.widgets
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
+import coil.size.Size
+import coil.transform.CircleCropTransformation
 import com.example.jetpackmovieapp.model.Movie
+import com.example.jetpackmovieapp.model.getMovies
 
 /**
  * Created by Deepak Rattan on 09/04/23
  */
 
 @Composable
-fun MovieRow(movie: Movie, onItemClick: (String) -> Unit) {
+fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit) {
     Card(
         modifier = Modifier
             .padding(4.dp)
@@ -48,10 +53,40 @@ fun MovieRow(movie: Movie, onItemClick: (String) -> Unit) {
                 shape = RectangleShape,
                 elevation = 4.dp
             ) {
-                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Image")
-            }
+                Image(
+                    contentDescription = "Image",
+                    painter = rememberImagePainter(
+                        data = movie.images[0],
+                        builder = {
+                            crossfade(true)
+                            transformations(CircleCropTransformation())
+                        }
+                    ),
+                )
 
-            movie.title?.let { Text(text = it) }
+//                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Image")
+            }
+            Column(modifier = Modifier.padding(4.dp)) {
+                movie.title?.let {
+                    Text(
+                        text = it,
+                        style = MaterialTheme.typography.h6
+                    )
+                }
+                movie.director?.let {
+                    Text(
+                        text = "Director : $it",
+                        style = MaterialTheme.typography.caption
+                    )
+                }
+                movie.year?.let {
+                    Text(
+                        text = "Release : $it",
+                        style = MaterialTheme.typography.caption
+                    )
+                }
+
+            }
 
         }
     }
