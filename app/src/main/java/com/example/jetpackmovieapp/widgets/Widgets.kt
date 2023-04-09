b767c882dd65cc4e5ae18c241f330030303c76cd
Deepak Rattan <deepak.rattan@itechnolabs.tech>
2023-04-09 17:23:47 +0530

    Adds expand functionality on the card to show more info -> Uses AnimatedVisibility

--- app/src/main/java/com/example/jetpackmovieapp/widgets/Widgets.kt
+++ app/src/main/java/com/example/jetpackmovieapp/widgets/Widgets.kt
@@ -1,5 +1,6 @@
 package com.example.jetpackmovieapp.widgets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -8,13 +9,22 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
 import coil.request.ImageRequest
 import coil.size.Size
@@ -28,18 +38,21 @@ import com.example.jetpackmovieapp.model.getMovies
 
 @Composable
 fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit) {
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp)
+//            .height(130.dp)
             .clickable {
                 movie.imdbID?.let { onItemClick(it) }
-            },
-        shape = RoundedCornerShape(
+            }, shape = RoundedCornerShape(
             corner = CornerSize(16.dp)
-        ),
-        elevation = 6.dp
+        ), elevation = 6.dp
     ) {
         Row(
             horizontalArrangement = Arrangement.Start,
@@ -55,40 +68,67 @@ fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit) {
             ) {
                 Image(
                     contentDescription = "Image",
-                    painter = rememberImagePainter(
-                        data = movie.images[0],
-                        builder = {
-                            crossfade(true)
-                            transformations(CircleCropTransformation())
-                        }
-                    ),
+                    painter = rememberImagePainter(data = movie.images[0], builder = {
+                        crossfade(true)
+                        transformations(CircleCropTransformation())
+                    }),
                 )
 
-//                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Image")
             }
             Column(modifier = Modifier.padding(4.dp)) {
                 movie.title?.let {
                     Text(
-                        text = it,
-                        style = MaterialTheme.typography.h6
+                        text = it, style = MaterialTheme.typography.h6
                     )
                 }
                 movie.director?.let {
                     Text(
-                        text = "Director : $it",
-                        style = MaterialTheme.typography.caption
+                        text = "Director : $it", style = MaterialTheme.typography.caption
                     )
                 }
                 movie.year?.let {
                     Text(
-                        text = "Release : $it",
-                        style = MaterialTheme.typography.caption
+                        text = "Release : $it", style = MaterialTheme.typography.caption
                     )
                 }
 
-            }
+                AnimatedVisibility(visible = expanded) {
+                    Column() {
+                        Text(buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                    fontSize = 13.sp
+                                )
+                            ) {
+                                append("Plot: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                    fontSize = 13.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            ) {
+                                movie.plot?.let { append(it) }
+                            }
+                        })
+                    }
+                }
 
+
+                Icon(imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                    contentDescription = "Down Arrow",
+                    tint = Color.DarkGray,
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            expanded = !expanded
+                        })
+            }
         }
+
+
     }
 
 }
